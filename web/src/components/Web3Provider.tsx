6793eca9c1c15da490835bfa93214959fa42bcde
ace <>
2024-04-28 22:28:42 +0800

    修改nft主页

--- web/src/components/Web3Provider.tsx
+++ web/src/components/Web3Provider.tsx
@@ -1,18 +1,38 @@
+'use client'
 
+import React, { ReactNode } from 'react'
+import { config, projectId } from '@/config'
 
-import { createWeb3Modal } from "@web3modal/wagmi/react";
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
-import { WagmiProvider } from "wagmi";
-import { projectId, metadata, chains } from '@/config/provider.config'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
-createWeb3Modal({ wagmiConfig, projectId, themeMode: 'light' });
+import { State, WagmiProvider } from 'wagmi'
 
-export default function App({ children }: any) {
+// Setup queryClient
+const queryClient = new QueryClient()
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+// Create modal
+createWeb3Modal({
+    wagmiConfig: config,
+    projectId,
+    enableAnalytics: true, // Optional - defaults to your Cloud configuration
+    enableOnramp: true // Optional - false as default
+})
+
+export default function Web3Provider({
+    children,
+    initialState
+}: {
+    children: ReactNode
+    initialState?: State
+}) {
     return (
-        <WagmiProvider config={wagmiConfig}>
-            {children}
+        <WagmiProvider config={config} initialState={initialState}>
+            <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
         </WagmiProvider>
-    );
+    )
 }
+
--- web/src/config.ts
+++ web/src/config.ts
@@ -1,10 +1,28 @@
-import { http, createConfig } from '@wagmi/core'
-import { mainnet, sepolia } from '@wagmi/core/chains'
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+import { cookieStorage, createStorage } from "wagmi";
 
-export const config = createConfig({
-    chains: [mainnet, sepolia],
-    transports: {
-        [mainnet.id]: http(),
-        [sepolia.id]: http(),
-    },
-})
+import { mainnet, sepolia } from "wagmi/chains";
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "";
+if (!projectId) throw new Error("Project ID is not defined");
+
+
+const metadata = {
+    name: "NFT租赁",
+    description: "NFT租赁",
+    url: "", // origin must match your domain & subdomain
+    icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const;
+export const config = defaultWagmiConfig({
+    chains,
+    projectId,
+    metadata,
+    ssr: true,
+    storage: createStorage({
+        storage: cookieStorage,
+    }),
+    // ...wagmiOptions, // Optional - Override createConfig parameters
+});
