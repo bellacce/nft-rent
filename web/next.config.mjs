298daa93b6d9296e12f3342575534c11fe811c01
ace <>
2024-05-26 13:24:58 +0800

    add crons

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/airports',
+        destination: 'https://nft-rent.vercel.app/api/airports',
+      },
+      {
+        source: '/api/flights',
+        destination: 'https://nft-rent.vercel.app/api/flights',
+      },
+    ];
+  },
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
   images: {
     remotePatterns: [
