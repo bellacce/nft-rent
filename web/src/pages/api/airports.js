4027d7a7b3b470c0ce2ecb63348c661bdc635a09
ace <>
2024-05-26 11:39:42 +0800

    add data

--- web/src/pages/api/airports.js
+++ web/src/pages/api/airports.js
@@ -1,5 +1,9 @@
-import airports from '../data/airports.json';
+import { promises as fsc } from 'fs';
+import path from 'path';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+    const jsonDirectory = path.join(process.cwd(), 'data');
+    const fileContents = await fsc.readFile(jsonDirectory + '/airports.json', 'utf8');
+    const airports = JSON.parse(fileContents);
     res.status(200).json(airports);
 }
--- web/src/pages/api/flights.js
+++ web/src/pages/api/flights.js
@@ -1,7 +1,11 @@
-import flights from '../data/flights.json';
+import { promises as fsc } from 'fs';
+import path from 'path';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     const { airportId, date } = req.query;
+    const jsonDirectory = path.join(process.cwd(), 'data');
+    const fileContents = await fsc.readFile(jsonDirectory + '/flights.json', 'utf8');
+    const flights = JSON.parse(fileContents);
     const filteredFlights = flights.filter(
         (flight) => flight.airportId === parseInt(airportId) && flight.date === date
     );
