953b02eed81449fbec1a19dbb29f217cfb0981ec
ace <>
2024-05-26 11:29:06 +0800

    remove examples

--- web/src/components/calculate/transfer/examples.ts
+++
@@ -1,6 +0,0 @@
-import { walletClient } from './config'
-
-const request = await walletClient.prepareTransactionRequest({
-    to: '0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
-    value: 1000000000000000000n
-})
